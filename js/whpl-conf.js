493b93905ab40b3089c09fa81e289fc668e33dbf
dshchung <dshchung@gmail.com>
2015-07-23 05:46:45 +0000

    production ready

--- js/whpl-conf.js
+++ js/whpl-conf.js
@@ -1,8 +1,8 @@
 var whplConf = {
-	widgetSrc: "https://dev.widget.wheepl.com:5001",
-	apiUrl: "https://dev.api.wheepl.com:5000/api/v1/",
-    debug: true,
-    ajaxUser: "username",
-    ajaxPass: "password",
+	widgetSrc: "https://widget.wheepl.com:5001",
+	apiUrl: "https://api.wheepl.com:5000/api/v1/",
+    debug: false,
+    ajaxUser: "3cSDoUbctQrYQTYkBen5idEHyQsJ6FCoZz3ZVGCd",
+    ajaxPass: "VHG2cQGmpEh9MsnrqKsPo7pfVxzZeHk43vKMoBoh",
     embedScript: "/js/whpl-embd.js"
 }
