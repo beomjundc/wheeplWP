dbf5e098ac9cd7e6fba4c6405ea70caaf923caf8
dshchung <dshchung@gmail.com>
2015-08-11 02:36:58 +0000

    prod ready

--- js/whpl-conf.js
+++ js/whpl-conf.js
@@ -1,6 +1,6 @@
 var whplConf = {
-	widgetSrc: "https://dev.widget.whee.pl:5001",
-	apiUrl: "https://dev.api.wheepl.com:5000/api/v1/",
-	debug: true,
-	embedScript: "/js/whpl-embd.js"
+	widgetSrc: "https://widget.whee.pl:5001",
+	apiUrl: "https://api.wheepl.com:5000/api/v1/",
+	debug: false,
+	embedScript: "/js/whpl-embd.min.js"
 }
